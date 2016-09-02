b54000ea667ae18f7884c39e4d18b9e2e942d345
Abhishek Anand <micky1991.abhishek@gmail.com>
2016-09-02 11:44:46 +0530

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='ownercheck',
-    version='0.1',
+    version='0.2',
     description='Verify ownership of domain and mobile apps using DNS and other methods',
     author='Fallible',
     author_email='hello@fallible.co',
     url='https://github.com/FallibleInc/ownercheck',
-    download_url='https://github.com/FallibleInc/ownercheck/tarball/0.1',
+    download_url='https://github.com/FallibleInc/ownercheck/tarball/0.2',
     packages=['ownercheck'],
     install_requires=[
         'dnspython',
